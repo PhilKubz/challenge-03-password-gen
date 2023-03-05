cbe8f35086d0b6355b0bee4da3682beff35d1d57
Phil <philip.kubisz@gmail.com>
2023-03-05 08:57:59 -0800

    Updated JS functionality to generate password / include comments

--- script.js
+++ script.js
@@ -1,66 +1,77 @@
-// Assignment code here
+// Variables set globally to be used for password creation
+var passLength = 8;
+var choiceArray = [];
+var specialCharArray = ['!', '@', '#', '$','%', '^','&', '*', '(', ')', '/', '/', '<', '>', '?'];
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCaseArray = ['A','B','C', 'D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var numberArray = ['1','2','3','4','5','6','7','8','9','0'];
 
-
-// Get references to the #generate element
+// set the #generate element in the DOM to have button functionality in JS
 var generateBtn = document.querySelector("#generate");
 
-var passComponents = {
-  lowercase: 'abcdefghijklmnopqrstuvwxyz',
-  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  number: '0123456789',
-  specialcharacters: "!@#$%^&*()_-+=<>/?[]{}\~`:;",
-};
 
-// Set functions that are later called
+// This function gathers user input on what to include/exclude for the new password
+function getPrompts(){
+  choiceArray = [];
+
+  // Check to create the length for desired new password to be generated
+  passLength = parseInt(prompt("What length would you like the password to be? (8 - 128 characters)"));
+  if (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    alert("Must be a number between 8 - 128 characters. Try Again.");
+  // A return false is included so that if the character requirement is not met, the function ends
+    return false;
+  }
+
+  // Check to include/exclude lowercase
+ if (confirm("Would you like to include lowercase characters?")) {
+    choiceArray = choiceArray.concat(lowerCaseArray);
+  }
+
+  // Check to include/exclude lowercase
+  if (confirm("Would you like to include uppercase characters?")) {
+    choiceArray = choiceArray.concat(upperCaseArray);
+  }
+
+  // Check to include/exclude numbers
+  if (confirm("Would you like to include numbers?")) {
+    choiceArray = choiceArray.concat(numberArray);
+  }
+
+  // Check to include/exclude special characters
+  if (confirm("Would you like to include special characters?")) {
+    choiceArray = choiceArray.concat(specialCharArray);
+  }
+
+  // The return true is included to show (especially in console) the function was run correctly
+  return true;
+}
+
+// generatePassword will gather information that we store in choiceArray to be used for password generation
 function generatePassword() {
-  var randompassword = "";
-
-  var passlength = prompt("What length would you like the password to be?(amount of characters)");
-  if (passlength < 8) {
-    alert("Length must be at least 8 characters");
+  var password = "";
+  for (var i = 0; i < passLength; i++) {
+    var randomCharacter = Math.floor(Math.random() * choiceArray.length);
+    password = password + choiceArray[randomCharacter];
   }
-  if (passlength > 128) {
-    alert("Length must be no more than 128 characters");
-  }
-  if (isNaN(passlength)) {
-    alert("Must be a number");
-  }
-
-  var lowercase = confirm("Would you like to include lowercase characters?");
-  if (lowercase) {
-    randompassword += passComponents.lowercase;
-  };
-
-  var uppercase = confirm("Would you like to include uppercase characters?");
-  if (uppercase) {
-    randompassword += passComponents.uppercase;
-  };
-
-  var numbers = confirm("Would you like to include numbers?");
-  if (numbers) {
-    randompassword += passComponents.number;
-  };
-
-  var specialcharacters = confirm("Would you like to include special characters?");
-  if (specialcharacters) {
-    randompassword += passComponents.specialcharacters;
-  };
-
-  for (let i = 0; i < length; i++) {
-    password += randompassword[Math.floor(Math.random() * randompassword.length)]
-  }
+  return password;
 }
 
 
-
-// Write password to the #password input
- function writePassword() {
-  var password = generatePassword();
+// writePassword will gather data from correctPrompts/user input to run generatePassword
+// This will then be used to set the passwordText as the newPassword we generate
+function writePassword() {
+  var CorrectPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  if(CorrectPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
 }
 
-// Add event listener to generate button
+
+
+// Event listener for the generate password button
 generateBtn.addEventListener("click", writePassword);
